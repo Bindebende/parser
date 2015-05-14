9b4cd958fd6f478f91be32e1316eb8a713ef419f
Bindebende <hoffmann.bende@gmail.com>
2015-05-14 19:42:08 +0200

    Initial Commit

---
+++ USB_command_parser/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  USB_command_parser
+//
+//  Created by Bendegúz Hoffmann on 2015. 05. 14..
+//  Copyright (c) 2015. Bendegúz Hoffmann. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
